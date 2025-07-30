d2b69a35248057f7734ac88ff56d26379963e1f4
filipolszewski <filipolszewsk@gmail.com>
2025-07-30 08:33:00 +0200

    Remove CMakeFiles from git index

--- CMakeFiles/run-fxe.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "*.qm"
-  "CMakeFiles/run-fxe"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run-fxe.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CMakeFiles/translations.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "*.qm"
-  "CMakeFiles/translations"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/translations.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
